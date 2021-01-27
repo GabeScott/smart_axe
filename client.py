5f6470997164f9b6f258342836705fc21aab72a4
Gabriel Scott <gabecscott@gmail.com>
2021-01-27 10:15:37 -0600

    Added webcam res change

--- client.py
+++ client.py
@@ -22,8 +22,8 @@ SOURCE_COORDS = [[213, 255], [934, 475], [234, 1512], [953, 1277]]
 DEST_COORDS = [[0,0],[703,0],[0,703],[703,703]]
 
 cap = cv2.VideoCapture('test.mp4')
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 num_detected = 0
 num_detected_in_a_row = 0
