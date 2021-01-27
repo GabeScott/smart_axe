9820bd152b70ee0174483327e012f5b11f12f47b
Gabriel Scott <gabecscott@gmail.com>
2021-01-27 10:14:22 -0600

    Ready for beta testing

--- client.py
+++ client.py
@@ -2,12 +2,24 @@ import requests
 import base64
 import json
 import cv2
+import numpy as np
 
 MIN_DETECT_FRAMES=2
 MIN_EMPTY_FRAMES=3
 
-url = 'http://3.85.90.159:8000'
+url = 'http://15.236.35.58:8000'
 myobj = {}
+
+#FOR 400x600
+#SOURCE_COORDS = [[105, 78], [359, 145], [93, 479], [366, 396]] 
+
+#FOR 720x1080
+#SOURCE_COORDS = [[46, 16], [760, 234], [36, 1248], [756, 1022]] 
+
+#FOR 1080x1920
+SOURCE_COORDS = [[213, 255], [934, 475], [234, 1512], [953, 1277]]
+
+DEST_COORDS = [[0,0],[703,0],[0,703],[703,703]]
 
 cap = cv2.VideoCapture('test.mp4')
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
@@ -17,11 +29,22 @@ num_detected = 0
 num_detected_in_a_row = 0
 num_empty_in_a_row = 0
 
+def transform_image(x, y, w, h, img):
+    M = cv2.getPerspectiveTransform(np.float32(SOURCE_COORDS),np.float32(DEST_COORDS))
+    dst = cv2.warpPerspective(img,M,(703,703))
+
+    points_to_transform = np.float32([[[x,y]], [[x+w/10, y+h]]])
+    transformed_points = cv2.perspectiveTransform(points_to_transform, M)
+
+    cv2.rectangle(dst, (transformed_points[0][0][0], transformed_points[0][0][1]), (transformed_points[1][0][0], transformed_points[1][0][1]), (0,255,0), 2)
+
+    return transformed_points
+
 
 def detect_axe(frame):
     frame_fixed = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE) 
 
-    dim = (400, 600)
+    dim = (1080, 1920)
     frame_fixed = cv2.resize(frame_fixed, dim, interpolation = cv2.INTER_AREA)
 
     cv2.imwrite('test-pic.jpg', frame_fixed)
@@ -37,12 +60,13 @@ while True:
     ret, frame = cap.read()
 
     boxes, frame = detect_axe(frame)
-    print(boxes)
+    # print(boxes)
 
     if len(boxes) > 0:
         num_detected_in_a_row += 1
         if num_detected_in_a_row == MIN_DETECT_FRAMES:
-            print("Detected at:"+str(boxes[0][0]) + ", " + str(boxes[0][1]))
+            transformed_points = transform_image(boxes[0][0], boxes[0][1], boxes[0][2], boxes[0][3], frame)
+            print("Detected at: ("+str(transformed_points[0][0][0]) + ", " + str(transformed_points[0][0][1]) + ")")
             cv2.imwrite("test-rotated"+str(num_detected)+".png", frame)
             num_detected += 1
             num_detected_in_a_row = 0
