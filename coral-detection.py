cec076cebdf36f6c2a841d33464b1b6e59c6071c
Gabriel Scott <gabecscott@gmail.com>
2021-02-11 12:12:43 -0600

    Bug fix

--- coral-detection.py
+++ coral-detection.py
@@ -10,7 +10,7 @@ import collections
 from threading import Thread
 
 MIN_DETECT_FRAMES=5
-MIN_EMPTY_FRAMES=20
+MIN_EMPTY_FRAMES=40
 
 DEBUG = 'debug' in sys.argv
 THROW_ONE_AXE = 'one_throw' in sys.argv
