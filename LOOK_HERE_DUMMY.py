c20fe73cf1e5e312ccad8601426e122f2fc8765a
Gabriel Scott <gabecscott@gmail.com>
2021-02-11 09:24:49 -0600

    Bug fix

--- LOOK_HERE_DUMMY.py
+++ LOOK_HERE_DUMMY.py
@@ -5,5 +5,5 @@ HIT_SOCKET = SocketIO('http://34.227.251.88', 3000)
 while True:
     x = input("Enter x: ")
     y = input("Enter y: ")
-    data = {'lane':0, 'x':x, 'y':y, 'width':10, 'height':10}
+    data = {'lane':0, 'x':x, 'y':y, 'width':2, 'height':50}
     HIT_SOCKET.emit("test hit", data)
